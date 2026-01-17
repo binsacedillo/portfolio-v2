72588d4660b210312e6af6dc3efa259a607a85c5
binsacedillo <beansgioacedillo@gmail.com>
2026-01-17 23:19:06 +0800

    feat(ui): display FEU Tech logo in Timeline [Audit Time: 2026-01-17 23:17:44]

--- src/components/landing/Timeline.tsx
+++ src/components/landing/Timeline.tsx
@@ -37,9 +37,8 @@ const timeline: TimelineEvent[] = [
     title: "FEU Tech - BS in Information Technology",
     description: "Currently pursuing Bachelor of Science in Information Technology with Web and Mobile Applications specialization at FEU Institute of Technology.",
     icon: <GraduationCap size={28} />,
-    // TODO: Uncomment and add your logo file to /public/feu-tech-logo.png
-    // logo: "/feu-tech-logo.png",
-    // logoAlt: "FEU Institute of Technology logo",
+    logo: "/feu-tech-logo.png",
+    logoAlt: "FEU Institute of Technology logo",
   },
 ];
 
